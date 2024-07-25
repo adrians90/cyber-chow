da3e4336f0d429f922237b87b64a24a0e26e03cc
adrians90 <adriansandu9090@gmail.com>
2024-07-25 16:07:05 +0100

    chore: Update page title and header styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="border-b-2 border-b-orange-500 py-6">
+    <div className="py-6 bg-[#000000] shadow-lg ">
       <div className="container mx-auto flex justify-between items-center">
         <Link
           to="/"
-          className="text-3xl font-bold tracking-tight text-orange-500"
+          className="text-3xl font-bold tracking-tight text-[#FFFF00]"
         >
           Cyber Chow
         </Link>

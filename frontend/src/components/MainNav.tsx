86391a567e282cf1f0e2d724cbeb8dff206ac1dd
adrians90 <adriansandu9090@gmail.com>
2024-07-26 14:59:48 +0100

    Update MainNav component to include Auth0 login functionality

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,8 +1,11 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <Button
+      onClick={async () => await loginWithRedirect()}
       variant="ghost"
       className="font-bold bg-[#FFFF00] hover:bg-[#FFFF00]/60 text-black"
     >

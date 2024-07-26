6595fb1029277edf26112a60a04b24340586472e
adrians90 <adriansandu9090@gmail.com>
2024-07-26 12:46:24 +0100

    chore: Add MainNav component and update Header

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold hover:text-black/60 hover:bg-[#ffff00]/80 bg-[#FFFF00]"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;

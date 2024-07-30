1d415210b453c14e8957a2dce1b9c27a3715e55f
adrians90 <adriansandu9090@gmail.com>
2024-07-30 17:22:49 +0100

    Add getCurrentUser endpoint to MyUserController

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -5,6 +5,8 @@ import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+
 // /api/my/user
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put(

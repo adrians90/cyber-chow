dd6209326fffe6c8f130cff5c6c664e3742978ee
adrians90 <adriansandu9090@gmail.com>
2024-08-01 15:59:26 +0100

    Improve form layout in DetailsSection component

--- frontend/src/forms/manage-restaurant-form/DetailsSection.tsx
+++ frontend/src/forms/manage-restaurant-form/DetailsSection.tsx
@@ -36,7 +36,7 @@ const DetailsSection = () => {
           control={control}
           name="city"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="flex-1">
               <FormLabel>City</FormLabel>
               <FormControl>
                 <Input {...field} className="bg-white" />
@@ -49,7 +49,7 @@ const DetailsSection = () => {
           control={control}
           name="country"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="flex-1">
               <FormLabel>Country</FormLabel>
               <FormControl>
                 <Input {...field} className="bg-white" />

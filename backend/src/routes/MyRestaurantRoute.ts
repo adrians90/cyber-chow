9af72bc541d78f43fa703428fb4e8d87cd6e7764
adrians90 <adriansandu9090@gmail.com>
2024-08-01 14:57:27 +0100

    chore: Add validation middleware for creating user's restaurant

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 
@@ -16,6 +18,10 @@ const upload = multer({
 router.post(
   "/",
   upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+
   MyRestaurantController.createMyRestaurant
 );
 

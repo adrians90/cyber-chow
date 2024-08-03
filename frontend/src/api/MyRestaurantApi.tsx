44c01f1e8b83ca9e3b4b85b02b1f462d4998a0c7
adrians90 <adriansandu9090@gmail.com>
2024-08-03 16:25:53 +0100

    use create my restaurant hook in manage restaurant page

--- frontend/src/api/MyRestaurantApi.tsx
+++ frontend/src/api/MyRestaurantApi.tsx
@@ -1,3 +1,4 @@
+import { Restaurant } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
 import { toast } from "sonner";
@@ -7,7 +8,9 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const useCreateMyRestaurant = () => {
   const { getAccessTokenSilently } = useAuth0();
 
-  const createMyRestaurantRequest = async (restaurantFormData: FormData) => {
+  const createMyRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
     const accessToken = await getAccessTokenSilently();
 
     const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {

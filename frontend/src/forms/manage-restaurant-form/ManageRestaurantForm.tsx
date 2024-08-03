2addc0b3b9a9187136d8e58ef2b16a8506884ac1
adrians90 <adriansandu9090@gmail.com>
2024-08-03 15:47:30 +0100

    Add image section to Manage Restaurant form

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -6,6 +6,9 @@ import DetailsSection from "./DetailsSection";
 import { Separator } from "@/components/ui/separator";
 import CuisinesSection from "./CuisinesSection";
 import MenuSection from "./MenuSection";
+import ImageSection from "./ImageSection";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -68,6 +71,16 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         <CuisinesSection />
         <Separator />
         <MenuSection />
+        <Separator />
+        <ImageSection />
+
+        {isLoading ? (
+          <LoadingButton />
+        ) : (
+          <Button className="text-[#FFFF00]" type="submit">
+            Submit
+          </Button>
+        )}
       </form>
     </Form>
   );

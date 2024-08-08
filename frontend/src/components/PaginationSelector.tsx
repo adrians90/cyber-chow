4415c957670250abe6c620e4380d459b105c694d
adrians90 <adriansandu9090@gmail.com>
2024-08-08 15:04:26 +0100

    Remove Previous pagination button when on Page 1

--- frontend/src/components/PaginationSelector.tsx
+++ frontend/src/components/PaginationSelector.tsx
@@ -22,9 +22,15 @@ const PaginationSelector = ({ page, pages, onPageChange }: Props) => {
   return (
     <Pagination>
       <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" onClick={() => onPageChange(page - 1)} />
-        </PaginationItem>
+        {page !== 1 && (
+          <PaginationItem>
+            <PaginationPrevious
+              href="#"
+              onClick={() => onPageChange(page - 1)}
+            />
+          </PaginationItem>
+        )}
+
         {pageNumbers.map((number) => (
           <PaginationItem>
             <PaginationLink

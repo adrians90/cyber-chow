e2000c896952a397dc4031569e97ce4f28bd3843
adrians90 <adriansandu9090@gmail.com>
2024-08-09 14:01:14 +0100

    Add MenuItem component for displaying menu items

---
+++ frontend/src/components/MenuItem.tsx
@@ -0,0 +1,19 @@
+import { MenuItem as MenuItemType } from "@/types";
+import { Card, CardContent, CardHeader } from "./ui/card";
+
+type Props = {
+  menuItem: MenuItemType;
+};
+
+const MenuItem = ({ menuItem }: Props) => {
+  return (
+    <Card className="cursor-pointer">
+      <CardHeader>{menuItem.name}</CardHeader>
+      <CardContent className="font-bold">
+        £{(menuItem.price / 100).toFixed(2)}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuItem;

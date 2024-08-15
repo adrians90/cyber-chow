722a8ea2879558e9c65ca717e4919a5e56d9a0a4
adrians90 <adriansandu9090@gmail.com>
2024-08-15 11:19:02 +0100

    Refactor OrderController and Restaurant model

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 import { Request, Response } from "express";
-import Restaurant from "../models/restaurant";
+import Restaurant, { MenuItemType } from "../models/restaurant";
 const stripe = new Stripe(process.env.STRIPE_API_KEY as string);
 
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -31,8 +31,40 @@ const createCheckoutSession = async (req: Request, res: Response) => {
     if (!restaurant) {
       throw new Error("Restaurant not found");
     }
+
+    const lineItems = createLineItems(
+      checkoutSessionRequest,
+      restaurant.menuItems
+    );
   } catch (error: any) {
     console.log(error);
     res.status(500).json({ message: error.raw.message });
   }
+};
+
+const createLineItems = (
+  checkoutSessionRequest: CheckoutSessionRequest,
+  menuItems: MenuItemType[]
+) => {
+  const lineItems = checkoutSessionRequest.cartItems.map((cartItem) => {
+    const menuItem = menuItems.find(
+      (item) => item._id.toString() === cartItem.menuItemId.toString()
+    );
+
+    if (!menuItem) {
+      throw new Error(`Menu item not found: ${cartItem.menuItemId}`);
+    }
+
+    const line_item: Stripe.Checkout.SessionCreateParams.LineItem = {
+      price_data: {
+        currency: "gbp",
+        unit_amount: menuItem.price,
+        product_data: {
+          name: menuItem.name,
+        },
+      },
+      quantity: parseInt(cartItem.quantity),
+    };
+    return line_item;
+  });
 };
